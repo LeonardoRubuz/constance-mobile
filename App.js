8a4dd220b29ae2dde5e14fd1c5ac3468cfb90a5d
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-04-20 15:04:09 +0100

    undone all previous changes

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import SmallButton from './components/Buttons/SmallButton';
-import LargeButton from './components/Buttons/LargeButton';
 
 export default function App() {
   return (
     <> 
       <View style={styles.container}>
-        <Header />
-        <View>
-          <SmallButton label="small button" theme="dark" />
-          <LargeButton label="large button" theme="dark" />
-        </View>
-        <Footer />
+        <Text>Constance shop</Text>
       </View>
       <StatusBar style="auto" />
     </>
